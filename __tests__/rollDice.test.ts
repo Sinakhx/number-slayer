e6c2006e9a5f52189437c3941d2a210cefe83741
Sina Khodabandehloo <khodabandehloo.sina@gmail.com>
2022-03-16 17:42:49 +0330

    test: rollDices

--- __tests__/rollDice.test.ts
+++ __tests__/rollDice.test.ts
@@ -5,10 +5,25 @@ describe("A suite for the 'rollDice' module", () => {
     it('should return a number', () => {
       expect(typeof rollDice()).toBe('number');
     });
+    it('should be a number be in range [1-6]', () => {
+      expect(rollDice()).toBeGreaterThanOrEqual(1);
+      expect(rollDice()).toBeLessThanOrEqual(6);
+    });
   });
   describe('rollDices', () => {
-    it('should return an Array', () => {
+    it('should return an Array of rolled dices', () => {
       expect(Array.isArray(rollDices())).toBe(true);
+      expect(rollDices(4).length).toBe(4);
+      rollDices(4).forEach(dice => {
+        expect(dice).toBeGreaterThanOrEqual(1);
+        expect(dice).toBeLessThanOrEqual(6);
+      });
+    });
+    it('should return an empty array if param is 0', () => {
+      expect(rollDices(0)?.length).toBe(0);
+    });
+    it('should throw if param is a negative value', () => {
+      expect(() => rollDices(-2)).toThrowError();
     });
   });
 });
