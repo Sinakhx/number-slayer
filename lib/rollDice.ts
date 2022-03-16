962c4b53d8f71d33dd2846a6c105690c689d375b
Sina Khodabandehloo <khodabandehloo.sina@gmail.com>
2022-03-17 02:30:27 +0330

    fix: prettier & lint-staged configs

--- lib/rollDice.ts
+++ lib/rollDice.ts
@@ -1,2 +1,5 @@
 export const rollDice = (): number => Math.floor(Math.random() * 6) + 1;
-export const rollDices = (count: number = 1): number[] => Array(count).fill(0).map(() => rollDice());
+export const rollDices = (count: number = 1): number[] =>
+    Array(count)
+        .fill(0)
+        .map(() => rollDice());
